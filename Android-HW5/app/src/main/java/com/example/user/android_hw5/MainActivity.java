70108a19fce39d7063d6f14bd8ddcdefb34d4792
user <julie0318.wang@gmail.com>
2017-05-02 00:30:19 +0800

    hw5

---
+++ Android-HW5/app/src/main/java/com/example/user/android_hw5/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.user.android_hw5;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView lv;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        ArrayList<AlbumItem> albumItem = new ArrayList<AlbumItem>();
+
+        albumItem.add(new AlbumItem(R.drawable.team1, "Bucks", "93", R.drawable.team2, "Raptors", "108"));
+        albumItem.add(new AlbumItem(R.drawable.team3, "Warriors", "128", R.drawable.team4, "Trail Blazers", "103"));
+
+        AlbumArrayAdapter adapter = new AlbumArrayAdapter(this, albumItem);
+        lv = (ListView)findViewById(R.id.lv);
+        lv.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
